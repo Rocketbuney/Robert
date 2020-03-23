98ef995453eaa466cd85214d9754c263e5f935fa
Jack Thake <jackthake@hotmail.com>
2020-03-23 11:33:50 -0700

    Added CSFML for control panel graphics

--- build.config.py
+++ build.config.py
@@ -1,23 +1,27 @@
 import os
 
 # use these flags for very strict C
-cflags = [ "-Wall", "-Wextra" ]
+cflags = [ "-Wall", "-Wextra", "-pedantic", "-Wdeprecated-declarations" ]
 lflags = [ "-lm" ]
 include = [ "lib/dyad/src" ]
 source = [ "lib/dyad/src" ]
 
 if "client" in opt:
-    include += [ "lib/microui/src" ]
-    source += [ "src/Control-Panel", "lib/microui/src" ]
+    lflags += [ "-L`pwd`/lib/CSFML/lib", "-lcsfml-graphics", "-lcsfml-window" ]
+    include += [ "lib/CSFML/include" ]
+    source += [ "src/Control-Panel" ]
     output = "robert-control-panel"
-    pass
+
 if "robot" in opt:
     lflags += [ "-lwiringPi", "-lncurses" ]
     source += [ "src/Robot" ]
     output = "robert"
-    pass
 
 if "sanitize" in opt:
     log("address sanitizer enabled")
     cflags += [ "-fsanitize=address" ]
     lflags += [ "-fsanitize=address" ]
+
+def post():
+    if "client" in opt:
+        os.system("install_name_tool -add_rpath @executable_path/lib/CSFML/lib/. robert-control-panel")
--- src/Control-Panel/main.c
+++ src/Control-Panel/main.c
@@ -1,8 +1,33 @@
-#include <stdio.h>
-#include "microui.h"
-#include "dyad.h"
+#include <SFML/Graphics.h>
 
-int main(void) {
+int main() {
+    sfVideoMode mode = {800, 600, 32};
+    sfRenderWindow* window;
+    sfEvent event;
 
-  return 0;
+    /* Create the main window */
+    window = sfRenderWindow_create(mode, "SFML window", sfResize | sfClose, NULL);
+    if (!window)
+        return 1;
+
+    /* Start the game loop */
+    while (sfRenderWindow_isOpen(window)) {
+        /* Process events */
+        while (sfRenderWindow_pollEvent(window, &event)) {
+            /* Close window : exit */
+            if (event.type == sfEvtClosed)
+                sfRenderWindow_close(window);
+        }
+
+        /* Clear the screen */
+        sfRenderWindow_clear(window, sfBlack);
+
+        /* Update the window */
+        sfRenderWindow_display(window);
+    }
+
+    /* Cleanup resources */
+    sfRenderWindow_destroy(window);
+
+    return 0;
 }
