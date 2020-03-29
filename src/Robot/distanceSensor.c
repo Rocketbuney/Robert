1130246c17a9d9094e121ee38400d4dcf2b6fce4
Jack Thake <jackthake@hotmail.com>
2020-03-28 23:25:20 -0700

    Fixed indentation

--- src/Robot/distanceSensor.c
+++ src/Robot/distanceSensor.c
@@ -8,90 +8,90 @@
 #include <unistd.h>
 
 void dist_initSensor(dist_sensorID sensor) {
-    int trig;
-    int echo;
+  int trig;
+  int echo;
 
-    switch(sensor) {
-        case front:
-            trig = 0;
-            echo = 4;
-            break;
-        case left:
-            trig = 3;
-            echo = 6;
-            break;
-        case right:
-            trig = 2;
-            echo = 5;
-            break;
-    }
+  switch(sensor) {
+    case front:
+      trig = 0;
+      echo = 4;
+      break;
+    case left:
+      trig = 3;
+      echo = 6;
+      break;
+    case right:
+      trig = 2;
+      echo = 5;
+      break;
+  }
 
-    pinMode(trig, OUTPUT);
-    pinMode(echo, INPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
 
-    digitalWrite(echo, 0);
-    delay(500);
+  digitalWrite(echo, 0);
+  delay(500);
 }
 
 float dist_getDistance(dist_sensorID sensor) {
-    int trig;
-    int echo;
+  int trig;
+  int echo;
 
-    float startTime;
-    float endTime;
+  float startTime;
+  float endTime;
 
-    switch(sensor) {
-        case front:
-            trig = 0;
-            echo = 4;
-            break;
-        case left:
-            trig = 3;
-            echo = 6;
-            break;
-        case right:
-            trig = 2;
-            echo = 5;
-            break;
-    }
+  switch(sensor) {
+    case front:
+      trig = 0;
+      echo = 4;
+      break;
+    case left:
+      trig = 3;
+      echo = 6;
+      break;
+    case right:
+      trig = 2;
+      echo = 5;
+      break;
+  }
 
-    digitalWrite(trig, 1);
-    delay(0.0001);
-    digitalWrite(trig, 0);
-    float startOfMeasurement = micros();
+  digitalWrite(trig, 1);
+  delay(0.0001);
+  digitalWrite(trig, 0);
+  float startOfMeasurement = micros();
 
-    while(digitalRead(echo) == 0  && micros() - startOfMeasurement < 30000)
-        startTime = micros();
+  while(digitalRead(echo) == 0  && micros() - startOfMeasurement < 30000)
+    startTime = micros();
 
-    while(digitalRead(echo) == 1)
-        endTime = micros();
+  while(digitalRead(echo) == 1)
+    endTime = micros();
 
-    return  (((endTime - startTime) / 2) / 29) < 0 ? 0 : ((endTime - startTime) / 2) / 29;
+  return  (((endTime - startTime) / 2) / 29) < 0 ? 0 : ((endTime - startTime) / 2) / 29;
 }
 
 void dist_cleanup(dist_sensorID sensor) {
-    int trig;
-    int echo;
+  int trig;
+  int echo;
 
-    switch(sensor) {
-        case front:
-            trig = 0;
-            echo = 4;
-            break;
-        case left:
-            trig = 3;
-            echo = 6;
-            break;
-        case right:
-            trig = 2;
-            echo = 5;
-            break;
-    }
+  switch(sensor) {
+    case front:
+      trig = 0;
+      echo = 4;
+      break;
+    case left:
+      trig = 3;
+      echo = 6;
+      break;
+    case right:
+      trig = 2;
+      echo = 5;
+      break;
+  }
 
-    pinMode(trig, OUTPUT);
-    pinMode(echo, OUTPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, OUTPUT);
 
-    digitalWrite(trig, 0);
-    digitalWrite(echo, 0);
+  digitalWrite(trig, 0);
+  digitalWrite(echo, 0);
 }
 #endif
--- src/Robot/motorController.c
+++ src/Robot/motorController.c
@@ -7,67 +7,67 @@ char motorFlags = 0x00;
 unsigned motorSpeed = 600;
 
 enum mot_pinAssignments { // Pin assignments for motor controller
-    AIN1 = 9,
-    AIN2 = 8,
-    BIN1 = 15,
-    BIN2 = 12,
-    PWMA = 1,
-    PWMB = 24,
-    STBY = 7
+  AIN1 = 9,
+  AIN2 = 8,
+  BIN1 = 15,
+  BIN2 = 12,
+  PWMA = 1,
+  PWMB = 24,
+  STBY = 7
 };
 
 void mot_initPins() {
-    pinMode(PWMA, PWM_OUTPUT);
-    pinMode(AIN1, OUTPUT);
-    pinMode(AIN2, OUTPUT);
-    pinMode(PWMB, PWM_OUTPUT);
-    pinMode(BIN1, OUTPUT);
-    pinMode(BIN2, OUTPUT);
-    pinMode(STBY, OUTPUT);
+  pinMode(PWMA, PWM_OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMB, PWM_OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(STBY, OUTPUT);
 }
 
 void mot_updatePins() {
-    if ((motorFlags & RIGHT_FORWARD) > 0) {
-        digitalWrite(BIN1, 0);
-        digitalWrite(BIN2, 1);
-    } else if((motorFlags & RIGHT_BACKWARD) > 0) {
-        digitalWrite(BIN1, 1);
-        digitalWrite(BIN2, 0);
-    }
+  if ((motorFlags & RIGHT_FORWARD) > 0) {
+    digitalWrite(BIN1, 0);
+    digitalWrite(BIN2, 1);
+  } else if((motorFlags & RIGHT_BACKWARD) > 0) {
+    digitalWrite(BIN1, 1);
+    digitalWrite(BIN2, 0);
+  }
 
-    if ((motorFlags & LEFT_FORWARD) > 0) {
-        digitalWrite(AIN1, 0);
-        digitalWrite(AIN2, 1);
-    } else if((motorFlags & LEFT_BACKWARD) > 0) {
-        digitalWrite(AIN1, 1);
-        digitalWrite(AIN2, 0);
-    }
+  if ((motorFlags & LEFT_FORWARD) > 0) {
+    digitalWrite(AIN1, 0);
+    digitalWrite(AIN2, 1);
+  } else if((motorFlags & LEFT_BACKWARD) > 0) {
+    digitalWrite(AIN1, 1);
+    digitalWrite(AIN2, 0);
+  }
 
-    if ((motorFlags & STBY_HIGH) > 0) {
-        digitalWrite(STBY, 1);
-    } else if ((motorFlags & STBY_LOW) > 0) {
-        digitalWrite(STBY, 0);
-    }
+  if ((motorFlags & STBY_HIGH) > 0) {
+    digitalWrite(STBY, 1);
+  } else if ((motorFlags & STBY_LOW) > 0) {
+    digitalWrite(STBY, 0);
+  }
 
-    pwmWrite(PWMB, motorSpeed);
-    pwmWrite(PWMA, motorSpeed * 1.25);
+  pwmWrite(PWMB, motorSpeed);
+  pwmWrite(PWMA, motorSpeed * 1.25);
 }
 
 void mot_cleanup() {
-    pinMode(PWMB, INPUT);
-    pinMode(BIN1, INPUT);
-    pinMode(BIN2, INPUT);
-    pinMode(AIN1, INPUT);
-    pinMode(AIN2, INPUT);
-    pinMode(PWMA, INPUT);
-    pinMode(STBY, INPUT);
+  pinMode(PWMB, INPUT);
+  pinMode(BIN1, INPUT);
+  pinMode(BIN2, INPUT);
+  pinMode(AIN1, INPUT);
+  pinMode(AIN2, INPUT);
+  pinMode(PWMA, INPUT);
+  pinMode(STBY, INPUT);
 
-    digitalWrite(PWMB, LOW);
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, LOW);
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, LOW);
-    digitalWrite(PWMA, LOW);
-    digitalWrite(STBY, LOW);
+  digitalWrite(PWMB, LOW);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(PWMA, LOW);
+  digitalWrite(STBY, LOW);
 }
 #endif
