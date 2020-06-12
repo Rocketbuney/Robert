bb4c0e4fd4e2c00e976e8049d7c860c153ef84cf
Jack Thake <jackthake@hotmail.com>
2020-06-11 22:54:39 -0700

    Basic use of distance sensor, verified by gdb, need full implementation

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "sys.h"
 
+#define ECHO 16
+#define TRIG 20
+
 size_t strlen(const char *s) {
   const char *p = s;
   while (*p)
@@ -9,12 +12,38 @@ size_t strlen(const char *s) {
 
 int robert_main(void) {
   ASSERT(gpio_ptr, "Failed to map GPIO memory\n");
-
   struct timespec sleepDuration = { 2, 0.0 };
+  struct timespec sensorDuration = { 0, 10000.0 };
+  struct timespec pulseStart, pulseEnd;
 
-  print("hello world\n");
+  long dist;
+
+  INP_GPIO(ECHO);
+  INP_GPIO(TRIG);
+  OUT_GPIO(TRIG);
   nanosleep(&sleepDuration, NULL);
-  print("hello world\n");
+
+  /* Pulse trigger pin */
+  GPIO_SET = 1 << TRIG;
+  nanosleep(&sensorDuration, NULL);
+  GPIO_CLR = 1 << TRIG;
+
+  /* Accurately get pulse start and end */
+  while(GET_GPIO(ECHO) == 0) {
+    clock_gettime(CLOCK_MONOTONIC_RAW, &pulseStart);  /* get time */
+  }
+
+  while(GET_GPIO(ECHO) == (1 << ECHO)) {
+    clock_gettime(CLOCK_MONOTONIC_RAW, &pulseEnd);  /* get time */
+  }
+
+  /* calculate distance */
+  long pulseEnd_s = (pulseEnd.tv_sec * 1000000) + (pulseEnd.tv_nsec / 1000L);
+  long pulseStart_s = (pulseStart.tv_sec * 1000000) + (pulseStart.tv_nsec / 1000L);
+  long long dur = (pulseEnd_s - pulseStart_s); /* in seconds */
+  dist = (dur / 2) / 29;
+
+  /* TODO: print dist */
 
   return 0;
 }
--- sys.h
+++ sys.h
@@ -5,6 +5,7 @@
 #define NULL 0L
 
 typedef unsigned short uint16_t;
+typedef unsigned long long uint64_t;
 typedef unsigned int uint32_t;
 typedef unsigned char uint8_t;
 typedef unsigned int size_t;
@@ -17,7 +18,9 @@ struct timespec {
 };
 
 /***** system calls ******/
+#define CLOCK_MONOTONIC_RAW 4
 int nanosleep(const struct timespec *req, struct timespec *rem);
+int clock_gettime(uint32_t clk_id, struct timespec *tp);
 
 /* File I/O */
 #define	O_RDWR 2
